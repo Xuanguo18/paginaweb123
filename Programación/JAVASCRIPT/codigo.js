d1d322da20df3f38e2c82e482da8418482346af4
Xuanguo18 <Xuanguo18@gmail.com>
2024-12-01 16:17:43 +0100

    Add files via upload

---
+++ Programación/JAVASCRIPT/codigo.js
@@ -0,0 +1,1 @@
+alert("Soy el primer script JS.");
---
+++ Programación/JAVASCRIPT/java1.js
@@ -0,0 +1,35 @@
+"curso de JavaScript".charAt(3) //devuelve la 'r'
+
+"curso de JavaScript".charCodeAt(11) 
+//esto devuelve 65 unicode de la letra 'a'
+
+"curso de JavaScript".indexOf("de") //devuelve 8
+
+"curso de JavaScript".lastIndexOf("a") //devuelve 13
+
+"curso de JavaScript".link("http://www.masterd.es") 
+//genera lo mismo que un <a herf ></a> de HTML
+
+"curso de JavaScript".concat(" de masterd "," centro de estudios")
+//devuelve “curso de JavaScript de masterd centro de estudios”
+
+String.fromCharCode(65,66,67,68,69) //devuelve ABCDE
+
+"curso de JavaScript".split(" ") //divide la cadena en 3 subcadenas
+
+"curso de JavaScript".slice(2,6) //devuelve “rso”
+
+"curso de JavaScript".substring(3,7) //devuelve “so d”
+
+"curso de JavaScript".substr(3,7) //devuelve “so de j”
+
+"curso de JavaScript".replace("JavaScript","js") 
+//devuelve “curso de js”
+
+"curso de JavaScript".search("vas") //devuelve 11
+
+"Curso De JavaScript".toLowerCase()
+// devuelve “curso de JavaScript“
+
+"curso de JavaScript".toUpperCase();
+//devuelve “CURSO DE JAVASCRIPT”
