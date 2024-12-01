55c3167248eefe4d440983314039d8a50ea72195
Xuanguo18 <Xuanguo18@gmail.com>
2024-12-01 23:04:55 +0100

    Delete Xuan_Guo_trabajoJAVASCRIPT directory

--- Xuan_Guo_trabajoJAVASCRIPT/js/galeria.js
+++
@@ -1,3 +0,0 @@
-const imagenes = ['img/galeria/9a0875da37f42392593749ee3102b5fb.jpg', 'img/galeria/grupo-personas-trabajando-plan-negocios-oficina_1303-15861.jpg', 'img/galeria/premium_photo-1664474834472-6c7d1e3198e2.jpg'];
-const galeriaDiv = document.getElementById('galeria');
-
